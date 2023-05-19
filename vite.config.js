95900eb07aa63a99d3972d5ffd0a65e515387521
jorge-73 <jorge_ap_73@hotmail.com>
2023-05-18 22:34:59 -0300

    Proyecto Final React Js CoderHause

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/jorge-73/ReactJs-Ecommerce/",
+  base: "https://jorge-73.github.io/ReactJs-Ecommerce/",
   plugins: [react()],
 })
